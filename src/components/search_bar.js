6724d6c2c2cf62ea26b78e5a15f0733404d53056
Kusum Ghimire <kusum.ghimire22@gmail.com>
2021-11-15 21:42:32 +0545

    constructor added to search_bar

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,10 +3,13 @@ import React, {Component} from 'react';
 class SearchBar extends Component{
     render(){
         return(
-            <input>
-            </input>
+            <input onChange={event => console.log(event.target.value)} />
+            
         )
     }
 }
 
 export default SearchBar;
+
+// states are used to save data
+// when the class is made it first use the constructor
