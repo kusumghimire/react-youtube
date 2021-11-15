e8b889aeddc06e1ba58640eaab85a3f2e93ee2e6
Kusum Ghimire <kusum.ghimire22@gmail.com>
2021-11-15 21:38:31 +0545

    searchbar impoerted in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import SearchBar from './components/search_bar';
 function App() {
   return (
     <div className="App">
       <h1>Hello there</h1>
+      <SearchBar />
     </div>
   );
 }
