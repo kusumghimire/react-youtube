b5defe7d15ea9a868a5bec602ef4755a47fefb7c
Kusum Ghimire <kusum.ghimire22@gmail.com>
2021-11-15 21:37:48 +0545

    class component added to search bar

---
+++ src/components/search_bar.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class SearchBar extends Component{
+    render(){
+        return(
+            <input>
+            </input>
+        )
+    }
+}
+
+export default SearchBar;
