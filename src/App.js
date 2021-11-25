bc225a5c0a1b78fd15febbea7d6078cb6e7aa33d
Kusum Ghimire <kusum.ghimire22@gmail.com>
2021-11-25 22:35:09 +0545

    youtube api search is added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import './App.css';
 import SearchBar from './components/search_bar';
-function App() {
+import YTSearch from 'youtube-api-search';
+import { Component } from 'react';
 
-  const API_KEY ='AIzaSyCCB-D6ee9HtNq2bJRdPMLmI6_VFEZfEBI';
-  return (
-    <div className="App">
+
+const API_KEY ='AIzaSyCCB-D6ee9HtNq2bJRdPMLmI6_VFEZfEBI';
+class App extends Component {
+  
+  constructor(props){
+    super(props)
+    YTSearch({key:API_KEY, term:'songs'},(data)=>){
+      console.log(data)
+    }
+  }
+  render() {
+    return (
+      <div>
+        <div className="App">
       <h1>Hello there</h1>
       <SearchBar />
     </div>
-  );
+      </div>
+    )
+  }
 }
 
 export default App;
