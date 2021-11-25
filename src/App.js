c6b052f792df31ce69ac000312c3aa270c6c2937
Kusum Ghimire <kusum.ghimire22@gmail.com>
2021-11-25 22:28:04 +0545

    api key added to app.js in javascript

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import SearchBar from './components/search_bar';
 function App() {
+
+  const API_KEY ='AIzaSyCCB-D6ee9HtNq2bJRdPMLmI6_VFEZfEBI';
   return (
     <div className="App">
       <h1>Hello there</h1>
