2f4cf74a078b99bd863d8b7a1cc1c50b2eaae274
Kusum Ghimire <kusum.ghimire22@gmail.com>
2021-11-25 23:04:16 +0545

    setState is added to search_bar.js component

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component{
+    constructor(props){
+       super(props)
+
+       this.state = {term:''}
+    }
     render(){
         return(
-            <input onChange={event => console.log(event.target.value)} />
-            
+            <div>
+
+                <input onChange={event => this.setState({term:event.target.value})} />
+                value of input:{this.state.term}
+            </div>
         )
     }
 }
@@ -13,3 +21,4 @@ export default SearchBar;
 
 // states are used to save data
 // when the class is made it first use the constructor
+// when we are using states there will be a class components
