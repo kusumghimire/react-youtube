a4fc0edfad876a10487e31217de576f6297bda16
Kusum Ghimire <kusum.ghimire22@gmail.com>
2021-11-29 15:37:42 +0545

    video list is imported in App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import './App.css';
-import SearchBar from './components/search_bar';
-import YTSearch from 'youtube-api-search';
-import { Component } from 'react';
+import  React,{ Component } from "react";
+import "./App.css";
+import SearchBar from "./components/search_bar";
+import YTSearch from "youtube-api-search";
+import VideoList from './components/video_list';
 
-
-const API_KEY ='AIzaSyCCB-D6ee9HtNq2bJRdPMLmI6_VFEZfEBI';
+const API_KEY = "AIzaSyCCB-D6ee9HtNq2bJRdPMLmI6_VFEZfEBI";
 class App extends Component {
-  
-  constructor(props){
-    super(props)
-    YTSearch({key:API_KEY, term:'songs'},(data)=>{
-      console.log(data)
-    })
+  constructor(props) {
+    super(props);
+    this.state = {videos:[] }
+    YTSearch({ key: API_KEY, term: "songs" }, (videos) => {
+      this.setState(videos);
+    });
   }
   render() {
     return (
       <div>
         <div className="App">
-      <h1>Hello there</h1>
-      <SearchBar />
-    </div>
+          <h1>Hello there</h1>
+          <SearchBar />
+        </div>
       </div>
-    )
+    );
   }
 }
 
